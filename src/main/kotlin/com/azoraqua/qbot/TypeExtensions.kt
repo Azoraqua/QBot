beb668c909772e402394738fe96e62c7f807b0df
Ronald Bunk <me@azoraqua.com>
2020-11-14 20:01:44 +0100

    Updated.
    Signed-off-by: Ronald Bunk <me@azoraqua.com>

--- src/main/kotlin/com/azoraqua/qbot/TypeExtensions.kt
+++ src/main/kotlin/com/azoraqua/qbot/TypeExtensions.kt
@@ -4,7 +4,7 @@ import net.dv8tion.jda.api.entities.Message
 import java.util.regex.Pattern
 
 internal val URL_PATTERN = Pattern.compile("https?://(www\\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\\.[a-zA-Z0-9()]{1,6}\\b([-a-zA-Z0-9()@:%_+.~#?&/=]*)")
-internal val IMAGE_PATTERN = Pattern.compile("((.*)(\\.))?(jpe?g|png|gif|bmp|webp)")
+internal val IMAGE_PATTERN = Pattern.compile("([a-zA-Z0-9\\-_.]+(\\.))?(jpe?g|png|gif|bmp|webp)")
 
 fun Message.isURL(): Boolean = URL_PATTERN.asMatchPredicate().test(this.contentRaw)
 fun Message.isImage(): Boolean = IMAGE_PATTERN.asMatchPredicate().test(this.contentRaw)
