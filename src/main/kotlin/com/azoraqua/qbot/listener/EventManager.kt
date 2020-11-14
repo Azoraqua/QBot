0990e67f6ae12cd32bfa7f74dba3f175992f719e
Ronald Bunk <me@azoraqua.com>
2020-11-14 17:08:35 +0100

    Updated.
    Signed-off-by: Ronald Bunk <me@azoraqua.com>

--- src/main/kotlin/com/azoraqua/qbot/listener/EventManager.kt
+++ src/main/kotlin/com/azoraqua/qbot/listener/EventManager.kt
@@ -11,11 +11,13 @@ import java.net.URL
 import java.nio.file.Files
 import java.util.*
 import java.util.concurrent.CompletableFuture
+import java.util.concurrent.Executors
 import java.util.concurrent.TimeUnit
 import java.util.regex.Pattern
 import javax.imageio.ImageIO
 
 class EventManager(val main: Main) : ListenerAdapter() {
+    internal val THREAD_POOL = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors() * 2)
     internal val LOGGER = HastebinLogger()
     internal val EXCEPTION_PATTERN = Pattern.compile("((.*)(\\.))?(.*)(Exception|Error)", Pattern.MULTILINE)
 
@@ -28,34 +30,39 @@ class EventManager(val main: Main) : ListenerAdapter() {
             return
         }
 
-        val text: String
+        THREAD_POOL.submit {
+            val text: String
 
-        if (event.message.attachments.isNotEmpty()) {
-            val file = event.message.attachments[0].downloadToFile().get()
+            if (event.message.attachments.isNotEmpty()) {
+                val file = event.message.attachments[0].downloadToFile().get()
 
-            if (IMAGE_PATTERN.asMatchPredicate().test(file.name)) {
-                text = main.tesseract.doOCR(file)
+                if (IMAGE_PATTERN.asMatchPredicate().test(file.name)) {
+                    text = main.tesseract.doOCR(file)
+                } else {
+                    text = Files.readString(file.toPath(), Charsets.UTF_8)
+                }
+
+                Files.delete(file.absoluteFile.toPath())
+            } else if (event.message.isImage()) {
+                text = main.tesseract.doOCR(ImageIO.read(URL(event.message.contentRaw)))
             } else {
-                text = Files.readString(file.toPath(), Charsets.UTF_8)
+                text = event.message.contentRaw
             }
 
-            Files.delete(file.absoluteFile.toPath())
-        } else if (event.message.isImage()) {
-            text = main.tesseract.doOCR(ImageIO.read(URL(event.message.contentRaw)))
-        } else {
-            text = event.message.contentRaw
-        }
+            THREAD_POOL.execute {
+                if (this.isException(text) || this.hasPrefix(text, "[WARN]", "[ERROR]")) {
+                    val url = CompletableFuture.supplyAsync {
+                        LOGGER.log(text)
+                    }.get()
 
-        if (this.isException(text) || this.hasPrefix(text, "[WARN]", "[ERROR]")) {
-            val url = CompletableFuture.supplyAsync {
-                LOGGER.log(text)
-            }.get()
-
-            event.message.addReaction("\uD83D\uDC40").queue {
-                event.message.delete().queueAfter(5, TimeUnit.SECONDS) {
-                    event.message.channel.sendTyping().queue {
-                        event.message.channel.sendMessage(url).queueAfter(3, TimeUnit.SECONDS) { m ->
-                            m.channel.sendMessage("${event.message.author.asMention} Please wait a moment. One of our staff will be with you anytime soon.").queue()
+                    event.message.addReaction("\uD83D\uDC40").queue {
+                        event.message.delete().queueAfter(5, TimeUnit.SECONDS) {
+                            event.message.channel.sendTyping().queue {
+                                event.message.channel.sendMessage(url).queueAfter(3, TimeUnit.SECONDS) { m ->
+                                    m.channel.sendMessage("${event.message.author.asMention} Please wait a moment. One of our staff will be with you anytime soon.")
+                                        .queue()
+                                }
+                            }
                         }
                     }
                 }
@@ -69,8 +76,9 @@ class EventManager(val main: Main) : ListenerAdapter() {
     }
 
     private fun hasPrefix(text: String, vararg prefixes: String): Boolean {
-        return Arrays.stream(prefixes).anyMatch { text.contains(it, true)
-            && !text.startsWith("~")
+        return Arrays.stream(prefixes).anyMatch {
+            text.contains(it, true)
+                && !text.startsWith("~")
         }
     }
 }
